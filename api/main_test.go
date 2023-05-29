1a9ab805cc38c6e13e8e508599a8cb2ea9e36c88
Pawel Banach <1054126+pawlobanano@users.noreply.github.com>
2023-05-29 15:32:21 +0200

    Update main_test.go

--- api/main_test.go
+++ api/main_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pawlobanano/UGF3ZcWCIEdvZ29BcHBzIE5BU0E/config"
+	"github.com/pawlobanano/NASA-Astronomy-Picture-of-the-Day-API/config"
 	"github.com/stretchr/testify/require"
 )
 
